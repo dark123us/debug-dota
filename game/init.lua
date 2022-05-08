9ed84fd92d1d746446692467cc06b438c2c4f778
Vadzim Pryhodzich <7676562@gmail.com>
2022-05-08 16:56:41 +0300

    fix

--- game/init.lua
+++ game/init.lua
@@ -1,6 +1,12 @@
-
 local args = {...}
 local directory = args[1]
 print('---directory---', directory)
 
+local logging = require(directory .. '.logging.logging')
+local dotalogging = require(directory..'.dotalogging')
 
+local units = {
+    logging=dotalogging
+}
+
+return units
